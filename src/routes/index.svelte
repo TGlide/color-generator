99f73a87b34d71c8305f1acfc06c4050fde2cb8a
tglide <26071571+TGlide@users.noreply.github.com>
2022-10-13 16:56:00 +0100

    feat: color gen step

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,6 +10,7 @@
 
 	let name = 'color';
 	let color = '';
+	let step = 10;
 
 	$: parsedColor = getHSLA(color);
 	$: [h, s] = parsedColor ?? ['0', '0'];
@@ -21,7 +22,7 @@
 			[baseVar]: `${h} ${s}`
 		};
 
-		for (let l = 10; l <= 100; l += 10) {
+		for (let l = step; l <= 100; l += step) {
 			const varName = `--palette-${colorName}-${l}`;
 			const varNameHsl = `${varName}-hsl`;
 
@@ -55,6 +56,10 @@
 					{#if color && parsedColor === null}
 						<Text color="red50" fontSize={12} mt={4}>Invalid color</Text>
 					{/if}
+				</Box>
+				<Box tag="label" display="block">
+					<Text fontSize={18} mb={8}>Step</Text>
+					<input bind:value={step} type="number" />
 				</Box>
 			</Flex>
 
